61a1a6fdaaa04962a8e4906095b17f23822ec75b
eddy08 <bhardwajharsh08@gmail.com>
2022-04-05 22:29:00 +0530

    🥇 Intialized Spring boot Project

---
+++ src/test/java/io/eddy08/liv2train/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.eddy08.liv2train.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
